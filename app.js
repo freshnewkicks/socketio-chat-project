62ca85bf7bd671d888815029e0759cb2de463baa
freshnewkicks <tylerdukedev@gmail.com>
2022-01-16 21:48:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('listening on *:3000');
+});
+
+module.exports = app;
