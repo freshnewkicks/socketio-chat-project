8c148576ef3019461a9804621c41406532bd33a4
freshnewkicks <tylerdukedev@gmail.com>
2022-01-16 22:37:17 -0500

    fixing chat

--- app.js
+++ app.js
@@ -17,12 +17,6 @@ const history = [];
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.once('chat message', () => {
-    for (hist of history) {
-      io.emit('chat message', hist);
-    }
-  })
-
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
